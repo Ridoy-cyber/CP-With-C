ef54f161c878e8f7e3c653840d3f3c9fedd088d8
Ridoy-cyber <67430311+Ridoy-cyber@users.noreply.github.com>
2022-05-21 17:43:17 +0600

    1st class task done

---
+++ 1st class/1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("1st Class valo Cilo ");
+    return 0;
+}
---
+++ 1st class/2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x =8,y=7,a=3,b=2;
+    int add=(x+y);
+    int div=(add/a);
+    int sub=(div*2);
+
+
+printf("%d\n",add,div,sub);
+return 0;
+
+
+}
